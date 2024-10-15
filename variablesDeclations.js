281249a74c4c278d084ded9f2ce62ad6c10527aa
vispz <142005659+Im-AhmadMunir@users.noreply.github.com>
2024-10-15 08:30:37 +0500

    variablesDeclarations

--- variablesDeclations.js
+++ variablesDeclations.js
@@ -3,6 +3,6 @@ let sum;
 
 const classNumber = 12;
 const camelCase = true;
-
+// Note: var uses hoisting and also its inheritance is function level
 var dontUseThisMuch = true;
 var onlyUseWhenNeeded = true;
