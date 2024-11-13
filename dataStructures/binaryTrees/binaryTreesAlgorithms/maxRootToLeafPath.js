376f511b131c53345a700be3bb97e34bb5bf6225
Ahmad Munir <contactahmadmunir@gmail.com>
2024-11-13 14:13:55 +0500

    aptupdate

--- dataStructures/binaryTrees/binaryTreesAlgorithms/maxRootToLeafPath.js
+++ dataStructures/binaryTrees/binaryTreesAlgorithms/maxRootToLeafPath.js
@@ -10,9 +10,38 @@ class Node {
   }
 }
 //! Solution :
+//* Using Depth First Recursively.
+
 const maxPathSum = (root) => {
     if(root === null) return -Infinity;
     if(root.left === null && root.right ===null) return root.val;
     const maxChildPathSum = Math.max(maxPathSum(root.left),maxPathSum(root.right));
     return root.val + maxChildPathSum;
 };
+
+const a = new Node(-1);
+const b = new Node(-6);
+const c = new Node(-5);
+const d = new Node(-3);
+const e = new Node(0);
+const f = new Node(-13);
+const g = new Node(-1);
+const h = new Node(-2);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+f.right = h;
+
+//        -1
+//      /   \
+//    -6    -5
+//   /  \     \
+// -3   0    -13
+//     /       \
+//    -1       -2
+
+console.log(maxPathSum(a)); // -> -8
